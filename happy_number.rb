96f14fe29d24bf6733fc9945c8f30c975f797a65
Gemille Ford <gemille.ford@gmail.com>
2015-05-05 16:44:07 -0400

    account for case where num is a single digit not equal to 1

--- happy_number.rb
+++ happy_number.rb
@@ -3,11 +3,10 @@ A happy number is a number defined by the following process:
 Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the procuess until the number equals 1, or it loops endlessly in a cycle which does not include 1.
 =end
 
-
 def digits(num)
   num = num.to_i
   digits_array = []
-  
+
   while num > 0
     digits_array << num % 10
     num /= 10
@@ -19,14 +18,14 @@ def happy_number(num)
   new_num = 0
   sequence = []
 
-  digits(num).each do |digit| 
+  digits(num).each do |digit|
     new_num += digit**2
     sequence << new_num
   end
 
   if new_num == 1
     puts "Yay, this is a happy number!"
-  elsif new_num == sequence[0]
+  elsif (new_num == sequence[0]) && (sequence.count != 1)
     puts "Sadly, this number is unhappy."
   else
     happy_number(new_num)
